7b48905e9f734630c42edc2fd1c163c636bfcb2c
jichani <jichan7571@naver.com>
2023-09-26 16:11:37 +0900

    sql 문구 추가 및 css / image들 추가

---
+++ sql/table.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `users` (
+	`user_id` INT NOT NULL AUTO_INCREMENT,
+	`user_email` VARCHAR(100) NOT NULL DEFAULT '',
+	`user_password` VARCHAR(1000) NULL DEFAULT '',
+	`user_name` VARCHAR(50) NULL DEFAULT '',
+	`user_img` VARCHAR(1000) NULL DEFAULT '',
+	`user_provider` VARCHAR(50) NULL DEFAULT '',
+	PRIMARY KEY (`user_id`),
+	UNIQUE INDEX `user_email` (`user_email`)
+)
+COLLATE='utf8mb4_general_ci'
+;
+
+CREATE TABLE `course` (
+	`course_id` INT NOT NULL AUTO_INCREMENT COMMENT '코스 고유 넘버',
+	`course_name` VARCHAR(100) NULL DEFAULT '' COMMENT '코스 이름',
+	`course_latitude` VARCHAR(1000) NOT NULL COMMENT '코스 위도',
+	`course_longitude` VARCHAR(1000) NOT NULL COMMENT '코스 경도',
+	`course_qr` VARCHAR(100) NOT NULL COMMENT '코스 QR 정보',
+	PRIMARY KEY (`course_id`)
+)
+COLLATE='utf8mb4_general_ci'
+;
+
+CREATE TABLE `users_course` (
+	`users_course_id` INT NOT NULL AUTO_INCREMENT,
+	`user_id` INT NOT NULL DEFAULT '0',
+	`course_id` INT NOT NULL DEFAULT '0',
+	PRIMARY KEY (`users_course_id`),
+	CONSTRAINT `FK_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `FK_course` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COLLATE='utf8mb4_general_ci'
+;
--- src/app.js
+++ src/app.js
@@ -9,12 +9,10 @@ const app = express();
 // express에게 ejs를 사용할 것이라고 선언
 app.set("view engine", "ejs");
 
-
 // view만 전달해주는 router view Router -> ejs 파일만 전달해주는 router
 app.use("/", viewRouter);
 // api만 전달해주는 router apiRouter -> 데이터만 전달해주는 router
 app.use("/api", apiRouter);
-
 
 // 위치는 절대 주소를 지정하고 파일 위치를 잡는다.
 app.set("views", process.cwd() + "/src/client/html");
