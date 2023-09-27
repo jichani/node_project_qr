cc673eec5295acf8f65fe942020752234d5c15fd
jichani <jichan7571@naver.com>
2023-09-27 11:55:35 +0900

    데이터베이스 연결 시작 | dotenv, mysql2 설치

---
+++ src/controller/courseController.js
@@ -0,0 +1,3 @@
+export const getCourseList = (req, res) => {
+  // 데이터베이스 정보 가져오기
+}
--- src/router/apiRouter.js
+++ src/router/apiRouter.js
@@ -2,7 +2,10 @@
 // api만 전달해주는 router apiRouter -> 데이터만 전달해주는 router
 
 import express from "express";
+import { getCourseList } from "../controller/courseController";
 
 const apiRouter = express.Router();
+
+apiRouter.get("/courses", getCourseList)
 
 export default apiRouter;
