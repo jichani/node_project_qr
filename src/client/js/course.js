fd5fb077f168c2d6c418e9de6961fdbb94221b23
jichani <jichan7571@naver.com>
2023-09-27 11:31:28 +0900

    build 수정

--- src/client/js/course.js
+++ src/client/js/course.js
@@ -1,5 +1,6 @@
 const locationMap = document.getElementById("location-map");
 let map;
+let markers = [];
 let isMapDrawn = false;
 let userLatitude;
 let userLongitude;
@@ -9,11 +10,26 @@ let userLongitude;
 const drawMap = (latitude, longitude) => {
   const options = {
     center: new kakao.maps.LatLng(latitude, longitude),
-    level: 5,
+    level: 2,
   };
   map = new kakao.maps.Map(locationMap, options);
   // map.setZoomable(false);
 };
+
+const deleteMarkers = () => {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+}
+
+const addUserMarker = () => {
+  let marker = new kakao.maps.Marker({
+    map: map,
+    position: new kakao.maps.LatLng(userLatitude, userLongitude),
+  });
+  markers.push(marker);
+}
 
 const addCourseMarker = () => {
   let markerImage = "/file/map_not_done.png";
@@ -22,19 +38,11 @@ const addCourseMarker = () => {
   const image = new kakao.maps.MarkerImage(markerImage, markerSize);
 
   const position1 = new kakao.maps.LatLng(35.79840347377205, 128.4930813305637);
-  const position2 = new kakao.maps.LatLng(35.80213901470637, 128.49690514633008);
 
   new kakao.maps.Marker({
     map: map,
     position: position1,
     title: "우리집",
-    image: image,
-  })
-
-  new kakao.maps.Marker({
-    map: map,
-    position: position2,
-    title: "화원교회",
     image: image,
   })
 };
@@ -43,6 +51,9 @@ const configurationLocationWatch = () => {
   if (navigator.geolocation) {
     // 위치가 이동될때마다 실행된다.
     navigator.geolocation.watchPosition((position) => {
+      // 초기화. 마커들을 지워준다.
+      deleteMarkers();
+
       userLatitude = position.coords.latitude;
       userLongitude = position.coords.longitude;
 
@@ -50,11 +61,12 @@ const configurationLocationWatch = () => {
         drawMap(userLatitude, userLongitude);
         isMapDrawn = true;
       }
+
+      // 유저 마커 그리기
+      addUserMarker();
     })
   }
 };
-
-
 
 
 configurationLocationWatch();
