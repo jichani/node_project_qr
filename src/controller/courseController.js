ccfdd01a968170e6803ca5332dfa8c124d64a2ec
jichani <jichan7571@naver.com>
2023-10-10 16:07:42 +0900

    안되서 준현 코드 받아서 수정..

--- src/controller/courseController.js
+++ src/controller/courseController.js
@@ -1,61 +1,67 @@
-import db from "../config/db";
+import db from "../config/db"
 
-export const getCourseList = async (req, res) => {
-  // 로그인했는지 여부를 판단한다. 그래서 유저 id를 가져온다. 로그인 안했으면 null.
-  const userId = req.user ? req.user.user_id : null;
+export const getCourseList = async (request, response) => {
+  // 로그인했는지 여부를 판단한다 그래서 유저 id를 가져온다 로그인 안했으면 null
+  const userId = request.user ? request.user.user_id : null;
 
-  // 데이터베이스에서 코스 정보와 방문 정보를 가져온다.
+  // 데이터베이스에서 코스 정보와 방문여부를 가져온다.
   const QUERY = `
-  SELECT c.*, uc.users_course_id
-  FROM course c
-  LEFT JOIN users_course uc
-  ON c.course_id = uc.course_id AND uc.user_id = ?`
-  // 데이터 베이스에 보내는 것 [userID]를 첫번째 ?에 집어넣는다. because, sql injection을 막기 위해서
-  const courseList = await db.execute(QUERY, [userId]).then((result) => result[0]);
+    SELECT c.*, uc.users_course_id 
+    FROM course c 
+    LEFT JOIN users_course uc 
+    ON c.course_id = uc.course_id
+    AND uc.user_id = ?`
 
-  res.json(courseList);
-};
+  const courseList = await db.execute(QUERY, [1]).then((result) => result[0]);
 
-export const qrCheck = async (req, res) => {
-  // TODO 임의로 유저 데이터 만듬
+  response.json(courseList);
+}
+
+export const qrCheck = async (request, response) => {
   const userId = 1;
 
-  // console.log(req.body);
-  const qrInfoData = req.body;
+  const qrInfoData = request.body;
+  console.log(qrInfoData);
 
-  // 검증코드 1 : 들어온 qr 코드에 해당하는 코스가 있는지
-  const QUERY1 = 'SELECT * FROM course WHERE course_qr = ?';
+  // 검증코드 1: 들어온 qr 코드에 해당하는 코스가 있는지 여부
+  const QUERY1 = `SELECT * FROM course WHERE course_qr = ?`
 
-  const course = await db.execute(QUERY1, [qrInfoData.qrCode])
-    .then((result) => result[0][0]);
+  const course = await db.execute(QUERY1, [qrInfoData.qrCode]).then((result) => result[0][0]);
+  if (!course) return response.status(400).json({ status: "not qrCode" });
 
-  if (!course) return res.status(400).json({ status: "not qrCode" });
+  // 검증코드 2: 해당유저 이 코스에 방문한적이 있는지
+  const QUERY2 = `SELECT * FROM users_course WHERE user_id = ? AND course_id = ?`
+  const userVisited = await db.execute(QUERY2, [userId, course.course_id]).then((result) => result[0][0]);
 
-  // 검증코드 2 : 해당 유저가 이 코스에 방문한 적이 있는지
-  const QUERY2 = 'SELECT * FROM users_course WHERE user_id = ? AND course_id = ?';
-  const userVisited = await db.execute(QUERY2, [userID, course.course_id])
-    .then((result) => result[0][0]);
+  if (userVisited) return response.status(400).json({ status: "already visited" });
 
-  if (userVisited) return res.status(400).json({ status: "visited" });
+  console.log("성공")
 
-  // 검증코드 3 : (수학) 반경 100m 내에 있을 때만 qr 코드 찍을 수 있음
-  calculateDistance(qrInfoData.latitude, qrInfoData.longitude, course.latitude, course.latitude);
+  // 검증코드 3 (수학) : 반경 100m내에 있을때만 qr코드 찍을 수 있음 - 선택
+  // dist m로 나옴
+  const dist = calculatorDistance(qrInfoData.latitude, qrInfoData.longitude, course.latitude, course.longitude)
 
-  console.log("성공");
-};
+  if (dist > 100) return response.status(400).json({ status: "distance over" });
 
-const calculateDistance = (currentLat, currentLon, targetLat, targetLon) => {
-  // 문자를 실수로 바꾸는 parseFloat 함수
-  currentLat = parseFloat(currentLat);
+  // 방문완료 - 데이터베이스에 추가
+  const QUERY3 = `INSERT INTO users_course (user_id, course_id) VALUES (?,?)`
+  await db.execute(QUERY3, [userId, course.course_id]);
+  return response.status(201).json({ status: "success" });
+}
+
+const calculatorDistance = (currentLat, currentLon, targetLat, targetLon) => {
+  currentLat = parseFloat(currentLat); // 35.875533...(문자) -> 35.875533...(실수)
   currentLon = parseFloat(currentLon);
   targetLat = parseFloat(targetLat);
   targetLon = parseFloat(targetLon);
 
-  // 111km. 위도 경도를 km로 바꾸기 위해서는 111000를 곱해주어야한다.
-  const dLat = (targetLat - currentLat) * 111000;
-  const dLon = (targetLon - currentLon) * 111000 * Math.cos(currentLat * (Math.PI / 180));
+  const dLat = (targetLat - currentLat) * 111000 // 111km
+  const dLon = (targetLon - currentLon) * 111000 * Math.cos(currentLat * (Math.PI / 180))
 
   return Math.sqrt(dLat * dLat + dLon * dLon);
-
-
 }
+
+
+/* 
+    controller => service (중요한 처리들) -> repository
+*/
